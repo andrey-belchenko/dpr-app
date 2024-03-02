241500f24ba7f6410a015fdf32f50ee43d87abc2
Andrey Belchenko <abelchenko@conteq.ru>
2024-03-03 02:40:35 +0300

    api

--- kotlin/exchange/exchange-lib/src/main/kotlin/scenario/model/data/Breaker.kt
+++
@@ -1,9 +0,0 @@
-//generated from profile
-package cc.datafabric.linesapp.scenario.model.data
-
-import cc.datafabric.linesapp.sys.model.ValueDelegate
-
-@Suppress("PropertyName", "unused")
-open class Breaker : ProtectedSwitch() {
-    var inTransitTime: Float? by ValueDelegate()
-}
