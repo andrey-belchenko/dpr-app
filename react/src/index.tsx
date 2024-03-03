18d5340ce5b17feca8ef9364133cb6c7cbdbeee1
Andrey Belchenko <abelchenko@conteq.ru>
2024-03-03 23:54:54 +0300

    react delete unused

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -4,24 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TestDiagram from './sandbox/TestDiagram';
 
-
-// const forceSlashAfterHash = () => {
-
-//   let _hash = window.location.hash;
-  
-//   if (_hash[1] && _hash[1] != '/') {
-
-//       window.location.href = window.location.origin + window.location.pathname + window.location.search + "#/" + _hash.slice(1);
-
-//   }
-// }
-
-// forceSlashAfterHash();
-
-
-// window.addEventListener('hashchange', forceSlashAfterHash);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -29,7 +12,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    {/* <TestDiagram/> */}
   </React.StrictMode>
 );
 
