dbdce1396f31dd456003db54fc0b1720499a9d3e
Andrey Belchenko <abelchenko@conteq.ru>
2024-03-04 02:23:03 +0300

    react create yantar in progress

--- react/src/common/components/index.tsx
+++ react/src/common/components/index.tsx
@@ -2,3 +2,4 @@ export { default as SideNavigationMenu } from './side-navigation-menu/SideNaviga
 export { default as Header } from './header/Header';
 export { default as Footer } from './footer/Footer';
 export { default as Hint } from './hint/Hint';
+export { default as Grid } from './grid/Grid';
